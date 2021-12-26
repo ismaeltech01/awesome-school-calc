9e7bc0c8b70e5d07f70513f11a9c8a296d51b5ff
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2021-12-26 10:17:29 -0600

    Minor changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,7 @@ export default function Footer() {
     <footer>
       <h2>Made by <a href="https://github.com/ismaeltovar">Ismael Tovar</a></h2>
       <h3>Licensed under the <a href="https://github.com/ismaeltovar/awesome-school-calc/blob/main/LICENSE">GPL-3.0</a></h3>
+      <p>NOTE: This calculator is JUST AN ESTIMATOR. Some figures provided by this tool MAY not be 100% accurate. I am not liable for any damages caused by actions taken because of or on the basis of the calculator's output.</p>
     </footer>
   );
 }
--- src/components/SemesterExam/Results.js
+++ src/components/SemesterExam/Results.js
@@ -1,21 +1,28 @@
+//SemesterExam results
 import React from "react";
-import { areAllVarsEmpty } from "./logic";
+import areAllVarsEmpty from "./logic";
 
 export default function Results(props) {
   const {usrSubmit, semesterAvg, percentEffect, desiredGrade, gradeNeeded} = props;
 
-  if (areAllVarsEmpty(semesterAvg, percentEffect, desiredGrade, gradeNeeded) && usrSubmit) {
-    return (
-      <p>You submitted invalid data! Please double check that you filled out ALL of the input fields.</p>
-    );
-  } else if (usrSubmit && gradeNeeded !== 0) {
-    return (
-      <p>To get a <strong>{desiredGrade}</strong> final semester average, you will need a score of atleast <strong>{gradeNeeded}</strong> on the semester exam.</p>
-    );
-  } else if (gradeNeeded === 0) {
-    return (
-      <p>To get a <strong>{desiredGrade}</strong> final semester average... you don't have to do anything! As long as you don't get a negative average, your good to go!</p>
-    );
+  if (usrSubmit) {
+    if (areAllVarsEmpty(semesterAvg, percentEffect, desiredGrade, gradeNeeded)) {
+      return (
+        <p>You submitted invalid data! Please double check that you filled out ALL of the input fields.</p>
+      );
+    } else if (gradeNeeded !== 0) {
+      return (
+        <p>To get a <strong>{desiredGrade}</strong> final semester average, you will need a score of atleast <strong>{gradeNeeded}</strong> on the semester exam.</p>
+      );
+    } else if (gradeNeeded === 0) {
+      return (
+        <p>To get a <strong>{desiredGrade}</strong> final semester average... you don't have to do anything! As long as you don't get a negative average, your good to go!</p>
+      );
+    } else {
+      return (
+        <p>Uncaught Error. Please try again. If error persists, contact the developer.</p>
+      );
+    }
   } else {
     return (
       <p>Fill out all of the fields above and click the Submit button to get a result.</p>
