61efcf8e76110b2a809fce80c58f71911ab9c87f
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2022-01-30 10:17:14 -0600

    Added back button to CalcForm

--- src/components/CalcForm/CalcForm.js
+++ src/components/CalcForm/CalcForm.js
@@ -2,21 +2,45 @@ import React from "react";
 import FormListItem from "./FormListItem";
 
 const CalcForm = (props) => {
-  const {onsubmit, onchange, itemData, helpData, submitText} = props;
+  const {onsubmit, onchange, itemData, helpData, submitText, extraBtn, extraBtnText, onBackClick} = props;
 
-  return(
-    <form onSubmit={onsubmit}>
-      <ul>
-        {
-          itemData.map(([name, text, minVal, maxVal, inputVal, stepVal], index) => (
-            <FormListItem key={name} itemName={name} text={text} onchange={onchange} minVal={minVal}
-            maxVal={maxVal} inputVal={inputVal} stepVal={stepVal} helpMsg={helpData[index]}/>
-          ))
-        }
-      </ul>
-      <button type="submit" id="submit-button">{submitText}</button>
-    </form>
-  );
+  if (typeof extraBtn !== 'boolean' || !extraBtn)
+    return(
+      <form onSubmit={onsubmit}>
+        <ul>
+          {
+            itemData.map(([name, text, minVal, maxVal, inputVal, stepVal], index) => (
+              <FormListItem key={name} itemName={name} text={text} onchange={onchange} minVal={minVal}
+              maxVal={maxVal} inputVal={inputVal} stepVal={stepVal} helpMsg={helpData[index]}/>
+            ))
+          }
+        </ul>
+        <button type="submit" id="submit-button">{submitText}</button>
+      </form>
+    );
+
+  if (extraBtn)
+    return(
+      <form onSubmit={onsubmit}>
+        <ul>
+          {
+            itemData.map(([name, text, minVal, maxVal, inputVal, stepVal], index) => (
+              <FormListItem key={name} itemName={name} text={text} onchange={onchange} minVal={minVal}
+              maxVal={maxVal} inputVal={inputVal} stepVal={stepVal} helpMsg={helpData[index]}/>
+            ))
+          }
+        </ul>
+        <button type="button" id="back-btn" onClick={onBackClick}>{extraBtnText}</button>
+        <button type="submit" id="submit-button">{submitText}</button>
+      </form>
+    );
 }
 
 export default CalcForm;
+
+const CustomElements = (props) => {
+  const {customEl} = props;
+
+
+  return(null);
+}
