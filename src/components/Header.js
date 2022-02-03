96ea53df1f7514ccc67c75d36b7aca7afd5604c5
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2022-02-03 10:49:01 -0600

    Updated App version num

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header>
       <NavLink to="/">
-        <h1 className="header-txt">Awesome School Calc  <sup className="header-sup-txt">v0.6.1 Alpha</sup></h1>
+        <h1 className="header-txt">Awesome School Calc  <sup className="header-sup-txt">v1.0.0</sup></h1>
       </NavLink>
     </header>
   );
