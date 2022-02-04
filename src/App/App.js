17d39b831389d1bcc6be4dae50e0b6623a2ca779
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2022-02-04 12:51:10 -0600

    Added theme context

--- src/App/App.js
+++ src/App/App.js
@@ -2,32 +2,48 @@ import '../css/styles';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {Header, Footer, InitialQuestion, SemesterExam, TestGrade, GPACalc, GPAWeightedCalc, GPAUnWeightedCalc, GPACustomCalc} from '../components';
+import { ThemeContext } from '../components/themeContext';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {theme: 'light', toggleTheme: this.toggleTheme};
+  }
+
+  toggleTheme = () => {
+    this.setState(state => ({
+      theme: state.theme === 'light' ? 'dark': 'light'
+    }));
+    console.log(this.state.theme);
   }
 
   render () {
     return (
-      <div className="App">
-        <Router>
-          <Header onclick={this.handleHeaderClick}/>
-          <Routes>
-            <Route path="/" element={<InitialQuestion/>}/>
-            <Route path="/semester-exam" element={<SemesterExam/>}/>
-            <Route path="/test-grade" element={<TestGrade/>}/>
-            <Route path="/gpa" element={<GPACalc/>}/>
-            <Route path="/gpa/weighted" element={<GPAWeightedCalc/>}/>
-            <Route path="/gpa/un-weighted" element={<GPAUnWeightedCalc/>}/>
-            <Route path="/gpa/custom" element={<GPACustomCalc/>}/>
-          </Routes>
-        </Router>
-        <Footer/>
-      </div>
+      <ThemeContext.Provider value={this.state}>
+        <AppContent />
+      </ThemeContext.Provider>
     );
   }
 }
 
 export default App;
+
+const AppContent = () => {
+  return(
+    <div className="App">
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<InitialQuestion/>}/>
+          <Route path="/semester-exam" element={<SemesterExam/>}/>
+          <Route path="/test-grade" element={<TestGrade/>}/>
+          <Route path="/gpa" element={<GPACalc/>}/>
+          <Route path="/gpa/weighted" element={<GPAWeightedCalc/>}/>
+          <Route path="/gpa/un-weighted" element={<GPAUnWeightedCalc/>}/>
+          <Route path="/gpa/custom" element={<GPACustomCalc/>}/>
+        </Routes>
+      </Router>
+      <Footer/>
+    </div>
+  );
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,24 @@
 import React from "react"; 
 import { NavLink } from "react-router-dom";
+import Switch from 'react-switch';
+import { ThemeContext } from "./themeContext";
+import { IoMoon, IoSunny } from "react-icons/io5";
 
 export default function Header() {
   return (
-    <header>
-      <NavLink to="/">
-        <h1 className="header-txt">Awesome School Calc  <sup className="header-sup-txt">v1.0.0</sup></h1>
-      </NavLink>
-    </header>
+    <ThemeContext.Consumer>
+        {({theme, toggleTheme}) => {
+          console.log({theme, toggleTheme});
+        return (
+          <header className={theme}>
+            <NavLink to="/">
+              <h1 id='header-txt' className={theme}>Awesome School Calc  <sup id='header-sup-txt' className={theme}>v1.0.0</sup></h1>
+            </NavLink>
+            <label>
+              <Switch onChange={toggleTheme} checked={theme === 'light' ? true: false} onColor='#e6e6e6' offColor='#646464' checkedIcon={<IoSunny/>} uncheckedIcon={<IoMoon/>}/>
+            </label>
+          </header>
+        )}}
+      </ThemeContext.Consumer>
   );
 }
