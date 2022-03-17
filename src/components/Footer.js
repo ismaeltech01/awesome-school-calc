1b4a7ecfc9ec0c2f2e17c35fed4af799a24db9f2
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2022-03-17 14:09:50 -0500

    Improved contrast ratio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ export default function Footer() {
  
   return (
     <footer className={theme}>
-      <h2 id="author-txt" className={theme}>Made by <a href="https://github.com/ismaeltovar">Ismael Tovar</a></h2>
-      <h3 id="license-txt" className={theme}>Licensed under the <a href="https://github.com/ismaeltovar/awesome-school-calc/blob/main/LICENSE">GPL-3.0</a></h3>
+      <h2 id="author-txt" className={theme}>Made by <a className={theme} href="https://github.com/ismaeltovar">Ismael Tovar</a></h2>
+      <h3 id="license-txt" className={theme}>Licensed under the <a className={theme} href="https://github.com/ismaeltovar/awesome-school-calc/blob/main/LICENSE">GPL-3.0</a></h3>
       <p id="notice-txt" className={theme}>NOTE: This calculator is JUST AN ESTIMATOR. Some figures provided by this tool MAY not be 100% accurate. I am not liable for any damages caused by actions taken because of or on the basis of the calculator's output.</p>
     </footer>
   );
